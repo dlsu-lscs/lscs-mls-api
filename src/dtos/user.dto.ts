73bf8c5008dd82a77ba7cef998f9d44bccfe3d0f
janaquino8 <janaquino53@gmail.com>
2025-10-23 23:45:48 +0800

    feat: create dtos for database models

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,1 +1,40 @@
-// User DTOs
+export interface Courses {
+    id: number,
+    classNumber: number,
+    courseName: string,
+    section: string,
+    remarks?: string
+}
+
+export interface CourseEnrollments {
+    id: number,
+    enrollCap?: number,
+    enrolled?: number,
+    status?: string,
+    courseId: number
+}
+
+export interface CourseTimeslots {
+    id: number,
+    day: string,
+    time: string,
+    room?: string,
+    instructor?: string,
+    courseId: number
+}
+
+export interface Users {
+    id: number,
+    idNumber: number,
+    email: string,
+    givenName: string,
+    familyName: string,
+    userId: string
+    pictureUrl: string
+}
+
+export interface SelectedCourses {
+    id: number,
+    courseId: number,
+    userId: number
+}
