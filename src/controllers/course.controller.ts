c24b23b8a47e6af7efe9c1df4bd087277a3790c0
janaquino8 <janaquino53@gmail.com>
2026-01-05 19:12:14 +0800

    fix: get all courses by course name route

--- src/controllers/course.controller.ts
+++ src/controllers/course.controller.ts
@@ -50,10 +50,10 @@ export async function getCourseById(req: Request, res: Response) {
 
 export async function getAllCoursesByCourseName(req: Request, res: Response) {
     try {
-        const name = req.query.q as string;
+        const name = req.body.courseName as string;
 
         if (!name) {
-            return res.status(400).json({ message: "Search query is empty" })
+            return res.status(400).json({ message: "No course name was given." })
         }
 
         const course = await CourseService.getAllCoursesByCourseName(name);
@@ -65,44 +65,5 @@ export async function getAllCoursesByCourseName(req: Request, res: Response) {
     } catch (err) {
         console.log(err);
         res.status(500).json({ message: 'Error fetching courses.', error: err });
-    }
-}
-
-export async function deleteCourse(req: Request, res: Response) {
-    try {
-        const id = Number(req.params.id);
-        if (isNaN(id)) {
-            return res.status(400).json({ message: 'Invalid user ID.' });
-        }
-
-        const success = await CourseService.deleteCourse(id);
-
-        if (!success) {
-            return res.status(404).json({ message: 'Course not found.' });
-        }
-        res.status(200).json({ message: 'Course deleted successfully.' })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: 'Error deleting course.', error: err });
-    }
-}
-
-export async function deleteCourseByCourseName(req: Request, res: Response) {
-    try {
-        const name = req.query.q as string;
-
-        if (!name) {
-            return res.status(400).json({ message: "Search query is empty" })
-        }
-
-        const success = await CourseService.deleteCourseByCourseName(name);
-
-        if (!success) {
-            return res.status(404).json({ message: 'Course/s not found.' });
-        }
-        res.status(200).json({ message: 'Course deleted successfully.' })
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ message: 'Error deleting course.', error: err });
     }
 }
--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -5,13 +5,10 @@ const router = Router();
 
 // Routes for the /courses endpoint
 router.post('/', CourseController.fetchCourses);
+router.get('/', CourseController.getAllCoursesByCourseName);
 
 // Routes for the /courses/:id endpoint
 router.get('/:id', CourseController.getCourseById);
-router.delete('/:id', CourseController.deleteCourse);
 
-// Routes for the /courses/search endpoint (search by course name)
-router.get('/search', CourseController.getAllCoursesByCourseName);
-router.delete('/search', CourseController.deleteCourseByCourseName);
 
 export default router;
