40f2fce342aa4d153805ab5559e18bcc6b6198c8
janaquino8 <janaquino53@gmail.com>
2026-05-08 16:43:36 +0800

    feat: change courses routes for clarity

--- src/routes/course.routes.ts
+++ src/routes/course.routes.ts
@@ -3,12 +3,14 @@ import * as CourseController from 'controllers/course.controller.js';
 
 const router = Router();
 
-// Routes for the /courses endpoint
-router.post('/', CourseController.fetchCourses);
-router.get('/', CourseController.getAllCoursesByCourseName);
+// Routes for the /courses/fetch endpoint
+router.post('/fetch', CourseController.fetchCourses);
 
-// Routes for the /courses/:id endpoint
-router.get('/:id', CourseController.getCourseById);
+// Routes for the /courses/search/:courseName endpoint
+router.get('/search/:courseName', CourseController.getAllCoursesByCourseName);
+
+// Routes for the /courses/id/:id endpoint
+router.get('/id/:id', CourseController.getCourseById);
 
 
 export default router;
